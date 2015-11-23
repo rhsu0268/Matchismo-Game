1d79e479e9abc1569fd65f0ee0438ff6589f7475
Richard Hsu <rhsu@gwu.edu>
2015-11-22 20:00:13 -0500

    Added more changes to cards

---
+++ Matchismo-Game/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchismo-Game
+//
+//  Created by Richard Hsu on 11/22/15.
+//  Copyright © 2015 Richard Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck: (Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+
+// no setter for the property
+@property (nonatomic, readonly) NSInteger score;
+
+@end
---
+++ Matchismo-Game/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo-Game
+//
+//  Created by Richard Hsu on 11/22/15.
+//  Copyright © 2015 Richard Hsu. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
